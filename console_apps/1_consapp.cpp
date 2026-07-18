6974395f143e9a3e14f13f406d8e74b0f881642b
mhmd2900 <mohamedsad2900@gmail.com>
2026-07-18 13:11:18 +0300

    Clean up files

---
+++ console_apps/1_consapp.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std ;
+
+
+int main ()
+{
+
+int arr [5] = { 5 , 2 , 1 , 2 , 6 } ;
+int arr2 [5] = {0} ;
+
+int rep = 0 ;
+int nc = -1 ;
+
+for ( int i = 0 ; i < 5 ; i ++ )
+{
+for ( int x = 0 ; x < 5 ; x ++ )
+{
+if ( arr[i] > arr[x] )
+rep ++ ;
+
+if ( arr[i] == arr[x] )
+nc ++ ;
+}
+arr2[rep - nc] = arr[i] ;
+rep = 0 ;
+nc = -1 ;
+
+}
+
+for ( int u = 0 ; u < 5 ; u++ )
+cout << arr2[u] << endl ;
+
+return 0;
+}
+
+
+
+
+
+
