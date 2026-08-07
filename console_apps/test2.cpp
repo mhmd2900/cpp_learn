1aa0a7d43ba494ebd9c15aa35ff82e7c39624ea6
mhmd2900 <mohamedsad2900@gmail.com>
2026-08-07 15:24:25 +0300

    recursion

--- console_apps/test2.cpp
+++ console_apps/test2.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int climb ( int stairs , int sum)
+int climb ( int total_stairs , int& trials )
 {
 
-// int a = 1 ;
-// int b = 2 ;
-// int sum = 0 ;
-if ( stairs == 0 )  { sum +=1 ; return 0 ; } 
-cout << 1 ;
+
+//int total_stairs ;       //  n
+int one_step  = 1 ;        //   n
+int two_steps = 2 ;       //   n
+int jump = 0 ;             //   
+//int trials = 0 ;        //   &
+int num = 0 ;             //
 
 
-return 1 + ( climb (stairs - 1 , sum ) ) ;
+
+if ( total_stairs <= 0 )  { trials +=1 ; return 0 ; } 
+//cout << 1 ;
+
+// if ( jump < 4 )
+jump = one_step + ( climb (total_stairs - one_step , trials ) ) ;
+
+// if (jump == 4 )
+// jump = jump + 1 + ( climb (total_stairs + 2 , trials ) ) ;
+
+// trials += 1 ;
+
+return trials ;
 
 
 }
-
+//  + 1 + ( climb (total_stairs - 2 , trials ) ) 
 
 
 int main() 
 {
-
- cout << climb ( 4 , 0 );
+int a = 0 ;
+cout << climb ( 4 , a );
 
     return 0;
 }
--- console_apps/test4.cpp
+++ console_apps/test4.cpp
@@ -1,14 +1,41 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include "../general/mlib.h"
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-
-int main ()
+int climb ( int total_stairs , int& trials )
 {
-mlib::reset_screen();
 
 
-return 0 ;
+//int total_stairs ; 
+int one_step  = 1 ;
+int two_steps = 2 ;
+int jump = 0 ;
+//int trials = 0 ;
+int num = 0 ;
+
+
+
+if ( total_stairs <= 0 )  { trials +=1 ; return 0 ; } 
+//cout << 1 ;
+
+// if ( jump < 4 )
+jump = 1 + ( climb (total_stairs - 1 , trials ) ) ;
+
+// if (jump == 4 )
+// jump = jump + 1 + ( climb (total_stairs + 2 , trials ) ) ;
+
+trials += 1 ;
+
+return trials ;
+
+
+}
+//  + 1 + ( climb (total_stairs - 2 , trials ) ) 
+
+
+int main() 
+{
+int a = 0 ;
+cout << climb ( 4 , a );
+
+    return 0;
 }
