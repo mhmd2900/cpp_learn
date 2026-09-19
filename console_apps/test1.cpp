e832e6020b4d421a11c2784bac58c13964b935dd
mhmd2900 <mohamedsad2900@gmail.com>
2026-09-19 15:59:22 +0300

    bank

--- console_apps/test1.cpp
+++ console_apps/test1.cpp
@@ -13,13 +13,14 @@ using namespace std ;
 struct strclientdata 
 {
 string name ;
-short pin ;
+int pin ;
 int balance ;
 };
 
-const string file_name = "E:/m5.txt";
+const string file_name = "E:/m5.txt";               //✅✅✅
 strclientdata stclientdata ;
-vector<strclientdata> vstclientdata ;
+vector<strclientdata> vst_write_records ;
+vector<strclientdata> vst_read_records ;
 
 //////////////////////////////////////////////////////////////////////////
 void  _clientdata_to_records ( )
@@ -34,7 +35,7 @@ cin >> stclientdata.pin ;
 cout << "enter balance \n";
 cin >> stclientdata.balance ;
 
-vstclientdata.push_back(stclientdata);
+vst_write_records.push_back(stclientdata);
 
 } while (mlib::want_to_repeat("add another client ?  \n    y   or   n   \n" ));
 
@@ -45,12 +46,12 @@ vstclientdata.push_back(stclientdata);
 void records_to_file_ ( string file_name )
 {
 fstream mfile ;
-mfile.open( file_name , ios::out | ios::app );
+mfile.open( file_name , ios::out );
 
 if ( mfile.is_open())
 {
-for ( auto& [ name , pin , balance ] : vstclientdata )  
-mfile << name << "#//#" << pin << "#//#" << balance << endl ;
+for ( strclientdata & data : vst_write_records )                  //✅✅✅
+mfile << data.name << "#//#" << data.pin << "#//#" << data.balance << endl ;
 
 mfile.close();
 }
@@ -59,65 +60,101 @@ mfile.close();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-vector<string> file_to_records( string file_name)
+strclientdata split_fields(string record)
 {
-vector<string>records ;
-string word ;
+strclientdata strecord ;
+string sep = "#//#";
+
+size_t pos = record.find(sep) ;
+strecord.name = record.substr( 0 , pos );
+record.erase(0 , pos+4);
+
+pos = record.find(sep) ;
+strecord.pin = stoi(record.substr( 0 , pos ));
+record.erase(0 , pos+4);
+
+strecord.balance = stoi(record);
+
+return strecord  ;
+}
+
+
+
+void file_to_records( string file_name)
+{
+
+string record ;
 fstream mfile ;
+
+strclientdata strecord ;
+vector<string> vrecord ;
+
 mfile.open(file_name , ios::in) ;
 if ( mfile.is_open())
 {
-while (getline ( mfile , word )) records.push_back(word);
+while (getline ( mfile , record ))    
+{
+strecord  = split_fields(record);
+vst_read_records.push_back(strecord);
+}
+
 mfile.close();
 }
-return records ;
+}
+
+
+
+void print_fields ( strclientdata clientdata)
+{
+
+
+    cout << "|"  << left << setw(5) << clientdata.name     << 
+            "|"  << left << setw(5) << clientdata.pin      <<
+            "|"  << left << setw(7) << clientdata.balance  << "|" << endl;
+    cout << "____________________" << endl ;
+    
+
 }
 
 
 
 
 
-            void record_to_fields_ ( string line )
-            {
-            vector<string> fields ;
-            string sep = "#//#";
-            size_t pos = 0 ;
 
-
-            while ((pos = line.find(sep)) != line.npos )
-            {
-            string copy = line.substr(0,pos) ;
-
-            if ( !line.empty())
-            cout << "|" << left << setw(5) << copy   ;
-
-            line.erase( 0 , copy.length()+4 );
-            }
-
-            if (!line.empty())
-            cout << "|" << left << setw(7) << line   ;
-
-            cout << "|" << endl ;
-            cout << "____________________" << endl ;
-
-            }
-
-
-
-
-
-void print_records_( vector<string>records )
+void print_records_( )
 {
-auto iter = records.begin();
-cout << "____________________" << endl ;
-cout << "|"  << left << setw(5) << "name"   << "|"  << left << setw(5) << "pin"  << "|"  << left << setw(7) << "balance"  << "|" << endl;
-cout << "____________________" << endl ;
+    cout << "____________________" << endl ;
+    cout << "|"  << left << setw(5) << "name"   << "|"  << left << setw(5) << "pin"  << "|"  << left << setw(7) << "balance"  << "|" << endl;
+    cout << "____________________" << endl ;
+    
 
-while ( iter != records.end() )
+auto iter = vst_read_records.begin();
+while ( iter != vst_read_records.end() )
 {
-record_to_fields_(*iter);
+print_fields(*iter);
 iter ++ ;
 }
+}
+
+
+
+
+void check_pin ( int num )
+{
+strclientdata client ;
+bool found = 0 ;                                     //✅✅✅
+
+for ( strclientdata& data : vst_write_records )
+{
+if ( data.pin == num )
+{
+client = data ;
+cout << " yes available  , name is " << client.name << " balance is " << client.balance << endl ;
+found = 1 ;
+}
+}
+if (!found)
+cout << " not available \n";
 }
 
 
@@ -127,10 +164,11 @@ iter ++ ;
 int main ()
 {
 _clientdata_to_records();
-
 records_to_file_ ( file_name );
 cout << endl ;
 
-vector<string>records = (file_to_records(file_name)) ;
-print_records_(records);
+file_to_records(file_name) ;
+print_records_();
+
+check_pin(99);
 }
